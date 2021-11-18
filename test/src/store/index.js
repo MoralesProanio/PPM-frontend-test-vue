328475b711ced509b3594a98fe3717d99f8f6e38
Michael Oswaldo Morales Proaño <54379483+MoralesMichael@users.noreply.github.com>
2021-11-18 03:24:48 -0500

    improve: better styles

--- test/src/store/index.js
+++ test/src/store/index.js
@@ -5,15 +5,131 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    welcome : true
+    welcome: true,
+    tickets: [
+      {
+        airline: 'Airline1',
+        description: 'Lorem ipsum is the text commonly used in graphic design in demos of typographies or design drafts to test the visual design before inserting the final text. Although he does not currently have sources to justify his hypotheses, the professor of philology',
+        price: '$2000',
+        duration: '55 min',
+        discount: '2%',
+        classFly: 'first',
+        stopover: 90
+      },
+      {
+        airline: 'Latam',
+        description: 'Lorem ipsum is the text commonly useds or design drafts to test the visual design before iessor of philology',
+        price: '$3000',
+        duration: '57 min',
+        discount: '2%',
+        classFly: 'first',
+        stopover: 30
+      },
+      {
+        airline: 'Airline2',
+        description: 'Lorem ipsum is ic des or design drafts to test the visual design before inserting the final text. Although he does not currently have sources to justify his hypotheses, the professor of philology',
+        price: '$4000',
+        duration: '444 min',
+        discount: '2%',
+        classFly: 'first',
+        stopover: 90
+      },
+      {
+        airline: 'Qtar',
+        description: 'Lorem ipsum is theesign in demos of typographies or design draesign before inserting the final text. Although he does not currently have sources to justify his hypotheses, the professor of philology',
+        price: '$5000',
+        duration: '100 min',
+        discount: '2%',
+        classFly: 'first',
+        stopover: 89
+      },
+      {
+        airline: 'Smms',
+        description: 'Lorem f typographies or design drafts to tesgn before inserting the final text.rrently have sources to justify his hypotheses, the professor of philology',
+        price: '$6000',
+        duration: '57 min',
+        discount: '2%',
+        classFly: 'first',
+        stopover: 1
+      },
+      {
+        airline: 'Rails',
+        description: 'phic design in demos of typographies or design drafts to test typotheses, the professor of philology',
+        price: '$7000',
+        duration: '23 min',
+        discount: '2%',
+        classFly: 'first',
+        stopover: 9
+      },
+      {
+        airline: 'VueAir',
+        description: 'Lorem ipsum is theraphic design in demos of typographies or design drafts to test the visual design before inserting the final text. Although he does not currently have sources to justify his hypotheses, the professor of philology',
+        price: '$8000',
+        duration: '54 min',
+        discount: '2%',
+        classFly: 'first',
+        stopover: 904
+      },
+      {
+        airline: 'AngularAir',
+        description: 'Lorem ipsum is the text commonlhies or design drafts to test the visual de justify his hypotheses, the professor of philology',
+        price: '$9000',
+        duration: '55 min',
+        discount: '2%',
+        classFly: 'first',
+        stopover: 905
+      },
+      {
+        airline: 'RestAirline',
+        description: 'Lorem ipsum is the s to test the visual design before inserting the fily have sources to justify his hypotheses, the professor of philology',
+        price: '$0000',
+        duration: '55 min',
+        discount: '2%',
+        classFly: 'first',
+        stopover: 904
+      },
+      {
+        airline: 'Airline67',
+        description: 'Lorem ipsum is the text commonly used in graphic design in demos of typographies or des visual design before inserting the final text. Although he does not currently have sources to justify his hypotheses, the professor of philology',
+        price: '$2000',
+        duration: '55 min',
+        discount: '2%',
+        classFly: 'first',
+        stopover: 908
+      },
+      {
+        airline: 'GotLess',
+        description: 'Lorem ipsum is the text commonly used in graphic design in demos ofurrently have sources to justify his hypotheses, the professor of philology',
+        price: '$2000',
+        duration: '55 min',
+        discount: '2%',
+        classFly: 'first',
+        stopover: 900
+      },
+      {
+        airline: 'FreshLine',
+        description: 'Lorem ipsum is the f typographies or design drafts to test the visual design before inserting the final text. Although he does not currently have sources to justify his hypotheses, the professor of philology',
+        price: '$2000',
+        duration: '55 min',
+        discount: '2%',
+        classFly: 'first',
+        stopover: 9
+      },
+    ]
   },
   mutations: {
-    disappear:state => {
+    disappear: state => {
       state.welcome = false;
     }
   },
-  actions: {
+  actions: {},
+  modules: {},
+  getters: {
+    findTickets: (state) => {
+      //here it is supposed to do searching method
+      return state.tickets;
+    }
   },
-  modules: {
-  }
+
 })
+
--- test/src/views/Search.vue
+++ test/src/views/Search.vue
@@ -1,79 +1,89 @@
 <template>
-  <div>
-    <div class="bg-img centered">
-      <b-card
-          class="small-card"
-      >
-        <h4 class="title">Find your perfect fly</h4>
-        <b-form class="form-floating" @submit="onSubmit" @reset="onReset" v-if="show">
-          <b-form-group
-              id="input-group-1"
-              label="Departure city:"
-              label-for="input-1"
-          >
-            <b-form-input
-                id="input-1"
-                v-model="form.departure"
-                placeholder="Quito"
-                required
-            ></b-form-input>
-          </b-form-group>
-
-          <b-form-group id="input-group-2"
-                        label="Destination City:"
-                        label-for="input-2">
-            <b-form-input
-                id="input-2"
-                v-model="form.arrive"
-                placeholder="Cuenca"
-                required
-            ></b-form-input>
-          </b-form-group>
-          <b-form-group id="input-group-3" label="Departure Date:" label-for="input-3">
-            <b-form-datepicker
-                id="ex-disabled-readonly"
-                v-model="form.departureDate"
-                requiredclear
-            >
-            </b-form-datepicker>
-          </b-form-group>
-          <b-form-group id="input-group-3" label="Return Date:" label-for="input-3">
-            <b-form-datepicker
-                id="ex-disabled-readonly"
-                v-model="form.returnDate"
-                requiredclear
-            >
-            </b-form-datepicker>
-          </b-form-group>
-          <b-button class="full-width" type="submit" variant="primary">Find a fly</b-button>&nbsp;
-        </b-form>
-      </b-card>
+  <div >
+    <div v-if="$store.state.welcome">
+      <img class="full-width" src="../assets/images/welcome.jpg" alt="welcome">
     </div>
-    <b-container fluid class="p-4 bg-dark bottom-footer">
-      <b-row>
-        <b-col>
-          <b-img class="small-img" thumbnail fluid src="https://picsum.photos/250/250/?image=54" alt="Image 1"></b-img>
-        </b-col>
-        <b-col>
-          <b-img class="small-img" thumbnail fluid src="https://picsum.photos/250/250/?image=58" alt="Image 2"></b-img>
-        </b-col>
-        <b-col>
-          <b-img class="small-img" thumbnail fluid src="https://picsum.photos/250/250/?image=59" alt="Image 3"></b-img>
-        </b-col>
-        <b-col>
-          <b-img class="small-img" thumbnail fluid src="https://picsum.photos/250/250/?image=61" alt="Image 3"></b-img>
-        </b-col>
-        <b-col>
-          <b-img class="small-img" thumbnail fluid src="https://picsum.photos/250/250/?image=54" alt="Image 1"></b-img>
-        </b-col>
-      </b-row>
-    </b-container>
+    <div  v-else>
+      <div class="centered full-width">
+        <b-card
+            class="small-card transparent-card"
+        >
+          <h4 class="title">Find your perfect fly</h4>
+          <b-form class="form-floating" @submit="onSubmit" @reset="onReset" v-if="show">
+            <b-form-group
+                id="input-group-1"
+                label="Departure City:"
+                label-for="input-1"
+            >
+              <b-form-input
+                  id="input-1"
+                  v-model="form.departure"
+                  placeholder="Quito"
+                  required
+              ></b-form-input>
+            </b-form-group>
+
+            <b-form-group id="input-group-2"
+                          label="Destination City:"
+                          label-for="input-2">
+              <b-form-input
+                  id="input-2"
+                  v-model="form.arrive"
+                  placeholder="Cuenca"
+                  required
+              ></b-form-input>
+            </b-form-group>
+            <b-form-group id="input-group-3" label="Departure Date:" label-for="input-3">
+              <b-form-datepicker
+                  id="ex-disabled-readonly"
+                  v-model="form.departureDate"
+                  required
+                  placeholder=""
+              >
+              </b-form-datepicker>
+            </b-form-group>
+            <b-form-group id="input-group-3" label="Return Date:" label-for="input-3">
+              <b-form-datepicker
+                  id="ex-disabled-readonly"
+                  v-model="form.returnDate"
+                  required
+                  placeholder=""
+              >
+              </b-form-datepicker>
+            </b-form-group>
+            <b-button class="full-width" type="submit" variant="primary">Find a fly</b-button>&nbsp;
+          </b-form>
+        </b-card>
+      </div>
+      <b-container fluid class="p-4 bg-dark bottom-footer">
+        <b-row>
+          <b-col>
+            <b-img class="small-img" thumbnail fluid src="https://picsum.photos/250/250/?image=54" alt="Image 1"></b-img>
+          </b-col>
+          <b-col>
+            <b-img class="small-img" thumbnail fluid src="https://picsum.photos/250/250/?image=58" alt="Image 2"></b-img>
+          </b-col>
+          <b-col>
+            <b-img class="small-img" thumbnail fluid src="https://picsum.photos/250/250/?image=59" alt="Image 3"></b-img>
+          </b-col>
+          <b-col>
+            <b-img class="small-img" thumbnail fluid src="https://picsum.photos/250/250/?image=61" alt="Image 3"></b-img>
+          </b-col>
+          <b-col>
+            <b-img class="small-img" thumbnail fluid src="https://picsum.photos/250/250/?image=54" alt="Image 1"></b-img>
+          </b-col>
+        </b-row>
+      </b-container>
+    </div>
   </div>
 </template>
 <script>
-
+import Header from "../components/Header";
 export default {
-  name: 'Search',
+  name: 'search',
+  components:{
+    Header
+  },
   data() {
     return {
       form: {
@@ -87,7 +97,8 @@ export default {
   },
   methods: {
     onSubmit(event) {
-      event.preventDefault()
+      event.preventDefault();
+      this.$router.push('detail');
     },
     onReset(event) {
       event.preventDefault()
